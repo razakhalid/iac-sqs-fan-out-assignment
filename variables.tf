885ea0f8a0be35f85203cd062f51384df332f347
Raza Khalid <rkhalid@kigo.io>
2025-05-03 11:42:31 -0500

    add access and secret key variables

--- variables.tf
+++ variables.tf
@@ -1,4 +1,15 @@
-# variables.tf
+variable "aws_access_key" {
+  description = "AWS access key"
+  type        = string
+  sensitive   = true
+}
+
+variable "aws_secret_key" {
+  description = "AWS secret key"
+  type        = string
+  sensitive   = true
+}
+
 variable "aws_region" {
   description = "AWS region to deploy resources"
   default     = "us-east-1"
